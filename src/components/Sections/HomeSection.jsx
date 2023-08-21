6df94cae09082b6c976f07b7759885329f7d08d7
Ama4538 <Voldableprism09@outlook.com>
2023-08-21 13:07:12 -0400

    Style home page

--- src/components/Sections/HomeSection.jsx
+++ src/components/Sections/HomeSection.jsx
@@ -1,8 +1,36 @@
+import '../../styles/main.css';
+import '../../styles/HomeSection.css';
+import nextArrow from '../../assets/icons/nextArrow.svg';
+
 const HomeSection = () => {
+  const nextImg = () => {
+
+  }
+
   return (
-    <>
-    <p>HomeSection</p>
-    </>
+    <section>
+      <div className='pictureContainer'>
+        <div className="picture">
+          <article className='picture-textWrapper'>
+            <h2 className='title default-text large-font'>Find Your Fashion</h2>
+            <h3 className='title default-text medium-font'> Captitvating and Subverting Expectations </h3>
+          </article>
+        </div>
+        <div className="picture">
+          <article className='picture-textWrapper'>
+            <h2 className='title default-text large-font'>Take Less to Impress</h2>
+            <h3 className='title default-text medium-font'> Life Changing, unique, pioneering </h3>
+          </article>
+        </div>
+        <div className="picture">
+          <article className='picture-textWrapper'>
+            <h2 className='title default-text large-font'>A Glimpse into Modern</h2>
+            <h3 className='title default-text medium-font'> The future is here, but are you? </h3>
+          </article>
+          <button className='button next'><img src={nextArrow} alt="Next arrow" /></button>
+        </div>
+      </div>
+    </section>
   )
 }
 
